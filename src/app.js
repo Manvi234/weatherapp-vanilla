dd55d395ef190cce98ff00d69aae3e4bceac8fba
Manvi234 <manvigawande234@gmail.com>
2022-12-19 12:59:26 +0530

    updated getForecast function

--- src/app.js
+++ src/app.js
@@ -22,12 +22,8 @@ function format_time(timestamp) {
   return ` ${day}   ${hours}:${minutes}`;
 }
 function getForecast(coordinates) {
-  console.log(coordinates);
-  let lat = coordinates.lat;
-  let lon = coordinates.lon;
-  let apiKey = `001bc651977f4b024af4d84282b0f02a`;
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 function displayTemperature(response) {
